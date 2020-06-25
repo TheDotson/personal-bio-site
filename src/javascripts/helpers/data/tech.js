12a67f5183acf560194c1ac4ef3d8fcac7126d22
Michael Dotson <thedotson@icloud.com>
2020-06-25 17:52:39 -0500

    updated project data and added tech data

---
+++ src/javascripts/helpers/data/tech.js
@@ -0,0 +1,18 @@
+const tech = [
+  {
+    name: 'HTML 5',
+    img: './assets/html5.png',
+  },
+  {
+    name: 'CSS 3',
+    img: './assets/css.png',
+  },
+  {
+    name: 'Javascript',
+    img: './assets/js.jpg',
+  },
+];
+
+const getTech = () => tech;
+
+export default { getTech };
