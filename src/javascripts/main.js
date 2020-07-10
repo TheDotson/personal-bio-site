543bf45d09cf67a3fa84213627d202259883ba2a
Michael Dotson <thedotson@icloud.com>
2020-07-10 16:07:01 -0500

    added bio builder function

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,10 @@
 import navbar from './components/navbar/navbar';
+import bio from './components/bio/bio';
 import '../styles/main.scss';
 
 const init = () => {
   navbar.buildNavBar();
+  bio.buildBio();
 };
 
 init();
